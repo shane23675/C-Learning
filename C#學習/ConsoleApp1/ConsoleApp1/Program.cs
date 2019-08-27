ea2e00bac8d4d34430bb431ae60871c384cb1a48
Helosole <kevin0976184751@gmai.com>
2019-08-27 22:42:45 +0800

    加入提示方法

--- C#學習/ConsoleApp1/ConsoleApp1/Program.cs
+++ C#學習/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Collections;
 
 namespace _1A2B
 {
     class Progress
     {
         private static string answer;
+        private static string record="";
+        private static int trial = 0;
         //產生隨機題目的方法
         public static string createAns()
         {
@@ -57,6 +60,7 @@ namespace _1A2B
         //檢查有幾A幾B的方法
         public static string CheckAB(string guess)
         {
+            trial++;
             int ACount = 0, BCount = 0;
             for (int i = 0; i < 4; i++)
             {
@@ -78,24 +82,122 @@ namespace _1A2B
                     }
                 }
             }
-            return string.Format("結果： {0}A{1}B", ACount, BCount);
+            //進行記錄
+            string result = string.Format("{0}A{1}B", ACount, BCount);
+            record += trial + ".\t" + guess + "\t" + result + "\n";
+            return result;
+        }
+        //顯示紀錄的方法
+        public static void ShowRecord()
+        {
+            Console.WriteLine("歷史紀錄：\n" + record);
+        }
+    }
+    //提示類：用來計算剩餘可能，協助作答
+    class Hint
+    {
+        private static ArrayList allPossibilities = new ArrayList();
+        private static ArrayList remainPossibilities = new ArrayList();
+        //初始化方法：產生所有可能的答案
+        public static void Init()
+        {
+            //產生四位數字串num
+            string num;
+            for (int i = 100; i < 9999; i++)
+            {
+                if (i < 1000)
+                {
+                    num = "0" + i;
+                }
+                else
+                {
+                    num = "" + i;
+                }
+                //檢測是否重複，無重複則加入allPossiblities
+                if (!Progress.CheckRepeat(num))
+                {
+                    allPossibilities.Add(num);
+                }
+            }
+        }
+        //過濾出可能答案的方法
+        public static void Filter(string guess, string result)
+        {
+            foreach (string p in allPossibilities)
+            {
+                int ACount = 0;
+                int BCount = 0;
+                for (int i=0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (p[i] == guess[j])
+                        {
+                            if(i == j)
+                            {
+                                ACount++;
+                            }
+                            else
+                            {
+                                BCount++;
+                            }
+                        }
+                    }
+                }
+                //判斷是否留下
+                if (ACount.ToString()[0] == result[0] && BCount.ToString()[0] == result[2])
+                {
+                    //AB值與result相同，可能為答案，加入remainPossibilities
+                    remainPossibilities.Add(p);
+                }
+            }
+            //結束篩選過程
+            allPossibilities = remainPossibilities;
+            remainPossibilities = new ArrayList();
+        }
+        //顯示出可能答案的方法
+        public static void ShowAllPossibilities()
+        {
+            Console.WriteLine("剩餘的可能答案：");
+            int count = 0;
+            foreach(string p in allPossibilities)
+            {
+                count++;
+                Console.Write(p + "\t");
+                if (count % 6 == 0)
+                {
+                    Console.Write("\n");
+                }
+            }
+            Console.WriteLine("");
         }
     }
     class Program
     {
+        //主程序
         static void Main()
         {
             //產生一個題目(在Progress內部)
             string ans = Progress.createAns();
-            //先輸出答案
-            Console.WriteLine(ans);
+            //初始化提示程序
+            Hint.Init();
+            //測試點：先輸出答案
+            //Console.WriteLine(ans);
             //開始遊戲
             while (true)
             {
                 Console.WriteLine("請輸入嘗試值：");
                 string guess = Console.ReadLine();
-                //檢查點：輸入exit可以退出循環
-                if (guess == "exit") { break; }
+                //輸入list：顯示歷史紀錄
+                if (guess == "list")
+                {
+                    Progress.ShowRecord();
+                }
+                //輸入hint：提示剩餘可能列表
+                else if (guess == "hint")
+                {
+                    Hint.ShowAllPossibilities();
+                }
                 //若輸入值不合法則輸出提示
                 else if (!Progress.CheckValid(guess))
                 {
@@ -108,14 +210,18 @@ namespace _1A2B
                 }
                 else
                 {
+                    //合法輸入，檢查對應AB值並返回結果
                     string result = Progress.CheckAB(guess);
+                    //進行提示篩選
+                    Hint.Filter(guess, result);
                     if (result == "4A0B")
                     {
                         Console.WriteLine("恭喜答對！！");
+                        break;
                     }
                     else
                     {
-                        Console.WriteLine(result);
+                        Console.WriteLine("結果： "+result);
                     }
                 }
             }
